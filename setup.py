391f30404f7c78b2cff14a429af89ce2bb88c0fd
vagrant <vagrant@ubuntu-oneiric.(none)>
2013-05-24 19:05:51 +0100

    Added version to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='entityfk',
     packages=find_packages(),  
     description = 'Django app that allows you to easily add a generic foreign key to a django model.',
     url = 'http://github.com/infoscout/entityfk',
-    #version = '0.1a.dev',    
+    version = '1.0',    
     install_requires=[
         'django==1.4',
     ]
