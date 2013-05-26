92895007dea9d53dd1596999f30a95593aadd5e7
Jon Brelig <jonbrelig@gmail.com>
2013-05-26 08:53:32 -0700

    Add version to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from isc_ops.setup_tools import setup, current_version
 
 setup(name='entityfk',
     packages=find_packages(),  
     description = 'Django app that allows you to easily add a generic foreign key to a django model.',
     url = 'http://github.com/infoscout/entityfk',
-    version = '1.0',    
+    version = current_version(),   
     install_requires=[
         'django==1.4',
     ]
