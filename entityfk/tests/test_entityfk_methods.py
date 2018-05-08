5f927bcdc7a971a0c5ff02db3da9495658a4fb10
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-08 00:16:15 +0000

    removed mock_our_model context manager from more tests

--- entityfk/tests/test_entityfk_methods.py
+++ entityfk/tests/test_entityfk_methods.py
@@ -10,53 +10,34 @@ from entityfk import providers, entityfk
 from entityfk.tests.models import AuthorTag, Book
 
 
-@contextmanager
-def mock_ourmodels():
-    with patch.object(providers.apps, 'get_models', return_value=[AuthorTag, Book]):
-        try:
-            old_providers = providers.providers
-            providers.providers = [providers.DjangoModelProvider()]
-            yield
-        finally:
-            providers.providers = old_providers
-
-
 class EntityFKMethodsTestCase(TestCase):
 
     def test_entity_label(self):
-        with mock_ourmodels():
-            self.assertEqual(entityfk.entity_label(Book(pk=1)), "tests.book")
+        self.assertEqual(entityfk.entity_label(Book(pk=1)), "tests.book")
 
     def test_entity_label_fail(self):
-        with mock_ourmodels():
-            with self.assertRaises(ValueError):
-                entityfk.entity_label("apple")
+        with self.assertRaises(ValueError):
+            entityfk.entity_label("apple")
 
     def test_entity_ref(self):
-        with mock_ourmodels():
-            self.assertEqual(entityfk.entity_ref(Book(pk=1)), ("tests.book", 1))
+        self.assertEqual(entityfk.entity_ref(Book(pk=1)), ("tests.book", 1))
 
     def test_entity_ref_fail(self):
-        with mock_ourmodels():
-            with self.assertRaises(ValueError):
-                entityfk.entity_ref("kisnyul")
+        with self.assertRaises(ValueError):
+            entityfk.entity_ref("kisnyul")
 
     def test_entity_model(self):
-        with mock_ourmodels():
-            self.assertEqual(entityfk.entity_model('tests.book'), Book)
+        self.assertEqual(entityfk.entity_model('tests.book'), Book)
 
     def test_entity_model_fail(self):
-        with mock_ourmodels():
-            with self.assertRaises(Exception):
-                entityfk.entity_model('tests.book2')
+        with self.assertRaises(Exception):
+            entityfk.entity_model('tests.book2')
 
     def test_entity_instance(self):
-        with mock_ourmodels():
-            with patch.object(Book.objects, 'get', return_value=None) as getter:
-                entityfk.entity_instance("tests.book", 1)
-                getter.assert_called_once_with(pk=1)
+        with patch.object(Book.objects, 'get', return_value=None) as getter:
+            entityfk.entity_instance("tests.book", 1)
+            getter.assert_called_once_with(pk=1)
 
     def test_entity_instance_fail(self):
-        with mock_ourmodels():
-            with self.assertRaises(Exception):
-                entityfk.entity_instance("tests.books", 1)
+        with self.assertRaises(Exception):
+            entityfk.entity_instance("tests.books", 1)
