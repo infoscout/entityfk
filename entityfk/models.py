99aa15afc23746abd8426379f86d18f2eba03d62
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-08 19:02:47 +0000

    importing sys

--- entityfk/models.py
+++ entityfk/models.py
@@ -1,6 +1,8 @@
+import sys
+
 import django
 
-if  sys.version_info[0] < 3:
+if sys.version_info[0] < 3:
     from providers import register_provider, DjangoModelProvider
 else:
     from .providers import register_provider, DjangoModelProvider
