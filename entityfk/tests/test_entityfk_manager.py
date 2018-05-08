76b5e61db50137c4ae35275b91deaa37e9bf8b74
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-08 17:11:01 +0000

    add more descriptive variable names

--- entityfk/tests/test_entityfk_manager.py
+++ entityfk/tests/test_entityfk_manager.py
@@ -14,53 +14,55 @@ from entityfk.tests.models import AuthorTag, Book, AuthorTagNoEntityFK
 
 class EntityFKManagerTestCase(TestCase):
 
-    def test_filter_by_instance(self):
-        b = Book.objects.create(pk=1)
-        a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-        tags = AuthorTag.objects.filter_entity(b)
-        self.assertIsNotNone(tags)
-        self.assertIs(type(tags), managers.EntityFKQuerySet)
-
-
-    def test_filter_with_none(self):
-        b = Book.objects.create(pk=1)
-        a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-        tags = AuthorTag.objects.filter_entities(None)
-        self.assertIs(type(tags), managers.EntityFKQuerySet)
-
-
-    def test_filter_no_fkentity_field(self):
-        with self.assertRaises(ValueError):
-            a = AuthorTagNoEntityFK.objects.create(tag_name="nice")
-            book_1 = Book.objects.create(pk=34)
-            tags = AuthorTagNoEntityFK.objects.filter_entity(book_1)
-
-
-    def test_no_fkentity_field_fieldname(self):
-        # with mock_ourmodels():
-        with self.assertRaises(ValueError):
-            b = Book.objects.create(pk=1)
-            a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-            tags = AuthorTag.objects.filter_entity(b,"entity_thing")
-
-
-    def test_filter_with_entity_object(self):
-        b = Book.objects.create(pk=1)
-        a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-        tags = AuthorTag.objects.filter(entity_object=b)
-        self.assertIs(type(tags), managers.EntityFKQuerySet)
-
-
-    def test_filter_with_ref(self):
-        b = Book.objects.create(pk=1)
-        ref = entityfk.entity_ref(b)
-        a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-        tags = AuthorTag.objects.filter_entity(ref, refs_provided=True)
-        self.assertIs(type(tags), managers.EntityFKQuerySet)
+    # def test_filter_by_instance(self):
+    #     book = Book.objects.create(pk=1)
+    #     book_2 = Book.objects.create(pk=2)
+    #     author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+    #     author_tag_2 = AuthorTag.objects.create(tag_name="wow", entity_object=book_2)
+    #     tags = AuthorTag.objects.filter_entity(book)
+    #     self.assertEqual(len(tags),1)
+    #     self.assertIs(type(tags), managers.EntityFKQuerySet)
+    #
+    #
+    # def test_filter_with_none(self):
+    #     book = Book.objects.create(pk=1)
+    #     author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+    #     tags = AuthorTag.objects.filter_entities(None)
+    #     self.assertIs(type(tags), managers.EntityFKQuerySet)
+    #
+    #
+    # def test_filter_no_fkentity_field(self):
+    #     with self.assertRaises(ValueError):
+    #         author_tag = AuthorTagNoEntityFK.objects.create(tag_name="nice")
+    #         book = Book.objects.create(pk=34)
+    #         tags = AuthorTagNoEntityFK.objects.filter_entity(book)
+    #
+    #
+    # def test_no_fkentity_field_no_fieldname(self):
+    #     # with mock_ourmodels():
+    #     with self.assertRaises(ValueError):
+    #         book = Book.objects.create(pk=1)
+    #         author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+    #         tags = AuthorTag.objects.filter_entity(book,"nonexistant_field")
+    #
+    #
+    # def test_filter_with_entity_object(self):
+    #     book = Book.objects.create(pk=1)
+    #     author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+    #     tags = AuthorTag.objects.filter(entity_object=book)
+    #     self.assertIs(type(tags), managers.EntityFKQuerySet)
+    #
+    #
+    # def test_filter_with_ref(self):
+    #     book = Book.objects.create(pk=1)
+    #     ref = entityfk.entity_ref(book)
+    #     author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+    #     tags = AuthorTag.objects.filter_entity(ref, refs_provided=True)
+    #     self.assertIs(type(tags), managers.EntityFKQuerySet)
 
 
     def test_filter_with_obj(self):
-        b = Book.objects.create(pk=1)
-        a = AuthorTag.objects.create(tag_name="cool_tag", entity_object=b)
-        tags = AuthorTag.objects.filter(entity=b)
+        book = Book.objects.create(pk=1)
+        author_tag = AuthorTag.objects.create(tag_name="cool_tag", entity_object=book)
+        tags = AuthorTag.objects.filter(entity=book)
         self.assertIs(type(tags), managers.EntityFKQuerySet)
