983d4fe027b0f2a7dc3048ccc626069fb86bab04
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-15 17:13:06 +0000

    add unicode literal support to python modules

--- entityfk/entityfk.py
+++ entityfk/entityfk.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 from django.db.models import signals
 from django.db.models.fields.related import RelatedField
