e4b80c97dfb7b9e5118b98174899cde40022e4e1
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-16 18:16:41 +0000

    fix un under-indentation and over-indentation

--- entityfk/providers.py
+++ entityfk/providers.py
@@ -72,9 +72,10 @@ class DjangoModelProvider(BaseProvider):
         return obj
 
     def to_ref(self, obj):
-        if (not isinstance(obj, Model)
-            and not (inspect.isclass(obj)
-                and issubclass(obj, Model))):
+        if (not isinstance(obj, Model) and
+            not (inspect.isclass(obj) and
+            issubclass(obj, Model))):
+            # if true, raise TypeNotSupport error 
             raise TypeNotSupported()
         label = "{}.{}".format(obj._meta.app_label, obj._meta.object_name)
         entity_label = label.lower()
