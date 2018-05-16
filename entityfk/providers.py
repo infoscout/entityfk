3c0519819d840815c3c160297842f59c01f8143e
Josie Lipkin <josie.lipkin@infoscoutinc.com>
2018-05-16 18:18:17 +0000

    try this indentation solution

--- entityfk/providers.py
+++ entityfk/providers.py
@@ -72,10 +72,8 @@ class DjangoModelProvider(BaseProvider):
         return obj
 
     def to_ref(self, obj):
-        if (not isinstance(obj, Model) and
-            not (inspect.isclass(obj) and
-            issubclass(obj, Model))):
-            # if true, raise TypeNotSupport error 
+        if (not isinstance(obj, Model) and not (inspect.isclass(obj) and
+                issubclass(obj, Model))):
             raise TypeNotSupported()
         label = "{}.{}".format(obj._meta.app_label, obj._meta.object_name)
         entity_label = label.lower()
